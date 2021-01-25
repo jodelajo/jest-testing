f8da7404781f2d7c07801907a521bc3f02252413
jodelajo <jodelajo@gmail.com>
2021-01-25 19:15:22 +0100

    write test for and implement add function

--- index.test.js
+++ index.test.js
@@ -1,3 +1,19 @@
-test('Even kijken og het werkt', () => {
-    console.log('Hallo, dit is een test, echt waar');
+function add(num1, num2){
+    // console.log('INPUT', num1, num2)
+    const sum = num1 + num2;
+    // console.log('SUM', sum);
+    return sum;
+}
+
+
+test('add() function  should retun the sum of 2 numbers', () => {
+    // console.log('Hallo, dit is een test, echt waar');
+
+    //Arrange
+    const num1 = 4;
+    const num2 = 5;
+    //Act
+    const sum = add(num1, num2);
+    //Assert
+    expect (sum).toBe(9)
 });
